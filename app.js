fa844fe68db3ee061feec5fe4b38511b035dc3da
Dakuan <dom.barker808@gmail.com>
2014-02-04 19:49:35 +0000

    just one

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ var cronJob = require('cron').CronJob,
     time = require('time'),
     loader = require('./loader/lib/loader');
 
-new cronJob('*/1 * * * *', function () {
+// new cronJob('*/1 * * * *', function () {
     loader.players(72);
-}, null, true, "Europe/London");
+// }, null, true, "Europe/London");
