e84a85c1abf99ce332ad0307fb32f71166744f2c
Dakuan <dom.barker808@gmail.com>
2014-02-04 21:17:55 +0000

    more exit

--- loader/tasks.js
+++ loader/tasks.js
@@ -2,6 +2,8 @@ var cronJob = require('cron').CronJob,
     time = require('time'),
     loader = require('./lib/loader');
 
-// new cronJob('*/5 * * * *', function () {
+new cronJob('*/5 * * * *', function () {
     loader.players(72);
-// }, null, true, "Europe/London");
+}, function () {
+    exit();
+}, true, "Europe/London");
