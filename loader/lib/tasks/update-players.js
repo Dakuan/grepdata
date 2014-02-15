1907bd2141caebff26507798fa58bea3199097c5
Dakuan <dom.barker808@gmail.com>
2014-02-15 09:50:33 +0000

    reset update players

--- loader/lib/tasks/update-players.js
+++ loader/lib/tasks/update-players.js
@@ -2,10 +2,7 @@ var loader = require('../loader').players,
     pruner = require('../pruner').players;
 
 function updatePlayers(world) {
-    loader(world)
-        .then(pruner);
+    loader(world).then(pruner);
 }
 
 module.exports = updatePlayers;
-
-updatePlayers(72)
