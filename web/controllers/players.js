083bb8bbd2be7a4c744d082d3f1533883c040679
Dakuan <dom.barker808@gmail.com>
2014-02-23 18:01:31 +0000

    css to pagination

--- web/controllers/players.js
+++ web/controllers/players.js
@@ -2,13 +2,12 @@ var repo = require('../repositories/players'),
     Q = require('q');
 
 function paginationViewModel(count, page, perPage) {
-    var next, prev = '',
+    var next, prev,
         nextPage = page + 1;
     if (page > 1) {
         prev = '/players?page=' + (--page);
     }
-    if (page < Math.ceil(count / perPage)) {
-
+    if (++page < Math.ceil(count / perPage)) {
         next = '/players?page=' + nextPage;
     }
     return {
@@ -39,7 +38,7 @@ module.exports = {
             repo.all(skip, perPage, {}, {
                 "sort": [['points', 'desc']]
             }),
-            repo.getTotal();
+            repo.getTotal()
         ]).then(function (results) {
             res.render('players/index', {
                 players: results[0],
